552e3d0b8f6235bb993802d3a1dd6d8c706d7012
Oihana Elizagoyen Ugalde <elizagoyen.131910@e.unavarra.es>
2023-05-28 16:25:10 +0200

    feat: Añadido el material generado en las prácticas 7 y 8

---
+++ MifareClassicTool/Mifare Classic Tool/app/src/test/java/de/syss/MifareClassicTool/TestUnitario.java
@@ -0,0 +1,13 @@
+package de.syss.MifareClassicTool;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class TestUnitario {
+    @Test
+    public void sumaDeDatosEsCorrecta() {
+        assertEquals(2, 1 + 1);
+    }
+}
