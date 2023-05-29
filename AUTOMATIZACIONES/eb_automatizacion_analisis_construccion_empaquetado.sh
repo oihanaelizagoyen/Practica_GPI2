2e9e4514ff992fb453b3dae277161da1be82205b
Oihana Elizagoyen Ugalde <elizagoyen.131910@e.unavarra.es>
2023-05-29 16:21:55 +0200

    fix: Corrección en la automatizacion del analisis, compilacion y empaquetado de los dos entregables que componen el proyecto

--- AUTOMATIZACIONES/eb_automatizacion_analisis_construccion_empaquetado.sh
+++ AUTOMATIZACIONES/eb_automatizacion_analisis_construccion_empaquetado.sh
@@ -1,10 +1,10 @@
 # Acccedemos a la carpeta del proyecto
 cd ../easybuggy/
 # Realizamos un análisis de código estático y guardamos el resultado en un fichero
-sudo /home/alumno/pmd-bin-7.0.0-rc2/bin/pmd check -d . -R rulesets/java/quickstart.xml -f text > ../ANALISIS_ESTATICO/analisis_easybuggy
+/home/alumno/pmd-bin-7.0.0-rc2/bin/pmd check -d . -R rulesets/java/quickstart.xml -f text > ../ANALISIS_ESTATICO/analisis_easybuggy
 # Realizamos la compilación y el empaquetado para generar el entregable
 mvn clean compile
 # Realizamos el empaquetado
 mvn package
 #Movemos el entregable a la carpeta correspondiente de entregables
-sudo cp ./target/ROOT.war ../ENTREGABLES/
+cp ./target/ROOT.war ../ENTREGABLES/
